a0048aaaa91adc4941fbf9c27aff40e4f259c3a0
Finc8888 <con8@mail.ru>
2018-06-19 15:56:27 +0300

    Adding regular expression in code

--- run_query.php
+++ run_query.php
@@ -11,36 +11,49 @@
   <?php
     require "../database_connection.php";
     $query_text = $_REQUEST["query"];
-    $query_resource = mysql_query(trim($query_text));
+    $query_resource = mysql_query($query_text);
 
 
     if (!$query_resource) {
       die("<h3>Ошибка выполнения SQL запроса '{$query_text}': ".mysql_error().
           "</h3>");
     }
-    $return_rows = false;
-    $lowcase_text = trim(strtolower($query_text));
-    $location = strpos($lowcase_text, "create");
-    if ($location===false||$location>0) {
-      $location = strpos($lowcase_text, "insert");
-      if ($location===false||$location>0) {
-        $location = strpos($lowcase_text, "update");
-        if ($location===false||$location>0) {
-          $location = strpos($lowcase_text, "delete");
-          if ($location===false||$location>0) {
-            $location = strpos($lowcase_text, "drop");
-            if ($location===false||$location>0) {
-              $return_rows = true;
-            }
-          }
-        }
-      }
+
+    // $lowcase_text = $query_text;
+
+    //Проверка без регулярного выражения
+    // $location = strpos($lowcase_text, "create");
+    // if ($location===false||$location>0) {
+    //   $location = strpos($lowcase_text, "insert");
+    //   if ($location===false||$location>0) {
+    //     $location = strpos($lowcase_text, "update");
+    //     if ($location===false||$location>0) {
+    //       $location = strpos($lowcase_text, "delete");
+    //       if ($location===false||$location>0) {
+    //         $location = strpos($lowcase_text, "drop");
+    //         if ($location===false||$location>0) {
+    //           $return_rows = true;
+    //         }
+    //       }
+    //     }
+    //   }
+    // }
+
+    //Проверка с регулярным выражением
+    //\s- эквивалентно [ \t\n\r]
+    $reg = "/^\s*(insert|update|delete|drop)/i";
+    $return_rows = true;
+    if(preg_match($reg, $query_text)){
+      $return_rows = false;
     }
+
+
+
     if($return_rows){
       echo "<h3>Результат вашего запроса <span>'{$query_text}' :</span></h3>";
       echo "<ul>";
       while ($row = mysql_fetch_row($query_resource)) {
-        echo "<li>{$row[0]}</li>";
+        echo "<li> {$row[0]} {$row[1]} {$row[2]} </li>";
       }
       echo "</ul>";
     }
